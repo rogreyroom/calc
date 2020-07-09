bbfabb7f988f77b48f48a9ffbafdb7b425954386
Robert Adamczewski <robert@adamczewski.me>
2020-07-09 17:40:18 +0200

    feat: add the computation function

--- src/js/modules/calculator.js
+++ src/js/modules/calculator.js
@@ -3,6 +3,42 @@ import {score, scoreOperations} from './selectors'
 import * as GET_ from './getters'
 import * as SET_ from './setters'
 
+// Helpers
+const computeValue = () => {
+  let computedValue
+  const prevValue = GET_.getPrevValue().toString().includes(',')
+                    ? parseFloat(GET_.getPrevValue().toString().replace(',','.'))
+                    : parseFloat(GET_.getPrevValue())
+
+  const currValue = GET_.getCurrValue().toString().includes(',')
+                    ? parseFloat(GET_.getCurrValue().toString().replace(',','.'))
+                    : parseFloat(GET_.getCurrValue())
+
+  switch (GET_.getCurrOperation()) {
+    case '+':
+      computedValue = prevValue + currValue
+      break
+    case '-':
+      computedValue = prevValue - currValue
+      break
+    case '*':
+      computedValue = prevValue * currValue
+      break
+    case '/':
+      computedValue = prevValue / currValue
+      break
+    case '%':
+      computedValue = (currValue / 100) * 1
+      break
+    default:
+      return
+  }
+  SET_.setCurrValue(computedValue)
+  SET_.setCurrOperation(null)
+  SET_.setPrevValue('')
+}
+
+
 // Operations
 const toggleNegativeValue = () => {
   SET_.setCurrValue(parseFloat(GET_.getCurrValue()) * -1)
@@ -26,12 +62,13 @@ export const getNumber = (number) => {
 export const getOperation = (operation) => {
     if (operation === 'C') return clearAll()
     if (operation === '+/-') return toggleNegativeValue()
+    if (GET_.getPrevValue() !== '' ) computeValue()
 
     SET_.setCurrOperation(operation)
     SET_.setPrevValue(GET_.getCurrValue())
     SET_.setCurrValue('')
 
-    console.log('Operation', 'Curr operation: ', GET_.getCurrOperation());
+    console.log('Operation', 'Curr operation: ', GET_.getCurrOperation(), 'Curr val: ', GET_.getCurrValue(), 'Prev val: ', GET_.getPrevValue());
 }
 
 // Display values
