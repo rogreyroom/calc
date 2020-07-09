e31b6cc419f7cecaf519a0a4b879073f36160b0b
Robert Adamczewski <robert@adamczewski.me>
2020-07-09 16:40:51 +0200

    feat: add modules for variables, selectors and listeners

---
+++ src/js/modules/selectors.js
@@ -0,0 +1,6 @@
+// All project html references
+
+export const scoreOperations = document.querySelector('.score-operations--js')
+export const score = document.querySelector('.score--js')
+export const operations = document.querySelectorAll('[data-operation]')
+export const numbers = document.querySelectorAll('[data-number]')
---
+++ src/js/modules/variables.js
@@ -0,0 +1,7 @@
+// All project global variables
+
+export let prevValue = ''
+export let currValue = ''
+export let currOperation = null
+export let equalOperation = false
+export let percentOperation = false
